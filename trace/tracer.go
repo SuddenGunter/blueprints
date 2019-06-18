c39d6f4b8c9427a9f4799f84efd4dd2ffdafdba2
Artem Kolomytsev <artemkolomytsev@gunter-macbook.Dlink>
2019-06-19 02:19:59 +0300

    added optional tracing by default

--- trace/tracer.go
+++ trace/tracer.go
@@ -19,6 +19,16 @@ func (t *tracer) Trace(a ...interface{}) {
 	fmt.Fprint(t.out, a...)
 	fmt.Fprintln(t.out)
 }
+
 func New(w io.Writer) Tracer {
 	return &tracer{out: w}
+}
+
+type nilTracer struct{}
+
+func (t *nilTracer) Trace(a ...interface{}) {}
+
+// Off creates a Tracer that will ignore calls to Trace.
+func Off() Tracer {
+	return &nilTracer{}
 }
