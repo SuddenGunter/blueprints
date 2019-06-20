e93065b95b8ffafd953ff065e1954cd89b34c619
Artem Kolomytsev <artemkolomytsev@gunter-mackbook.local>
2019-06-21 00:41:12 +0300

    added users avatarts support to chat

--- chat/auth.go
+++ chat/auth.go
@@ -49,7 +49,8 @@ func loginHandler(w http.ResponseWriter, r *http.Request) {
 		}
 		fmt.Printf("user data: %v", user)
 		authCookieValue := objx.New(map[string]interface{}{
-			"name": user.Name,
+			"name":       user.Name,
+			"avatar_url": user.AvatarURL,
 		}).MustBase64()
 		http.SetCookie(w, &http.Cookie{
 			Name:  "auth",
