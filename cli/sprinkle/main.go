ea322be5dd5aaa860f38ffd3757e558072c2423e
Artem Kolomytsev <kolomytsev1996@gmail.com>
2019-06-23 01:03:55 +0300

    sprinkle templates fix: added spaces. domainify/.gitignore added

--- cli/sprinkle/main.go
+++ cli/sprinkle/main.go
@@ -13,13 +13,13 @@ const otherWord = "*"
 
 var transforms = []string{
 	otherWord,
-	otherWord + "app",
-	otherWord + "site",
-	otherWord + "time",
-	"get" + otherWord,
-	"go" + otherWord,
+	otherWord + " app",
+	otherWord + " site",
+	otherWord + " time",
+	"get " + otherWord,
+	"go " + otherWord,
 	"lets " + otherWord,
-	otherWord + "hq",
+	otherWord + " hq",
 }
 
 func main() {
